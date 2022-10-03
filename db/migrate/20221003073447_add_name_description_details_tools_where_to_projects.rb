5c83ecd2860d4dbf2d97251d5d9e30b327ea6394
Maxime Gueye <gueye.maxime1994@gmail.com>
2022-10-03 09:35:45 +0200

    migration for projects created

---
+++ db/migrate/20221003073447_add_name_description_details_tools_where_to_projects.rb
@@ -0,0 +1,9 @@
+class AddNameDescriptionDetailsToolsWhereToProjects < ActiveRecord::Migration[7.0]
+  def change
+    add_column :projects, :name, :string
+    add_column :projects, :description, :string
+    add_column :projects, :details, :string
+    add_column :projects, :tools, :string
+    add_column :projects, :where, :string
+  end
+end
